6d225c75930ca4b16042e22c2de6dc1effbff40e
Sheila Hanson <slhanson11@live.com>
2024-02-08 14:39:48 -0600

    added utils children

---
+++ controllers/api/homeRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { Blog, User } = require('../../models');
+
+// Get all Blogs
+router.get('/', async (req.res) => {
+  try {
+    const db
+  }
+})
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,11 @@
 module.exports = {
-  // Format as mm/dd/yyyy
+ 
   format_time: (date) => {
+    // Format as mm/dd/yyyy
     return date.toLocalTimeString();
-  }
+  },
+  format_amount: (amount) => {
+    // Format numbers with commas
+    return parseInt(amount).toLocaleString();
+  },
 };
