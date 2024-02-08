c33e80d48ea444c929a8f3c9b3b12fdd81f84b07
Sheila Hanson <slhanson11@live.com>
2024-02-08 15:30:00 -0600

    updated homeRoutes.js

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,9 +1,27 @@
-const router = require('express').Router();
-const { Blog, User } = require('../../models');
+const router = require("express").Router();
+const { Blog, User } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // Get all Blogs
-router.get('/', async (req.res) => {
+router.get("/", async (req, res) => {
   try {
-    const db
+    // Get all projects and join with user data
+    const dbBlogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const blogs = dbBlogData.map((blog) => blog.get({ plain: true }));
+
+    res.render("homepage", {
+      blogs,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
-})
+});
