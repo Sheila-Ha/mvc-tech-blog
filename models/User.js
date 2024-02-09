558721f1eceadb261f835fe2a9a74e2a19313bca
Sheila Hanson <slhanson11@live.com>
2024-02-09 14:22:25 -0600

    updated models user.js

--- models/User.js
+++ models/User.js
@@ -1,46 +1,55 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/jconnection');
-//const bcrypt = 
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
-// WEEK 13 #17 USE INFO
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+   }
+  }
 User.init(
   {
     id: {
-
-
-
-
+      type: DataTypes.INTEGER,
+      allowNull: false;
+      primaryKey: true,
+      autoIncrement: true,
     },
    username: {
-
+    type: DataTypes.STRING,
+    allowNull: false,
    },
    email: {
-
-
-
-
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
    },
    password: {
-
-   },
-
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [6],
+    },
   },
 },
 {
   hooks: {
-
-    beforeCreate: 
+    async beforeCreate(newUserData) {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10);
+    },
   },
-  {
-    beforeUpdate:
-  },
-},
-sequelize: ,
-timestamp: ,
-freezeTableName: ,
-underscored: ,
-modelName: '',
+  // {
+  //   beforeUpdate:
+//   },
+// },
+sequelize,
+timestamp: false,
+freezeTableName: true,
+underscored: true,
+modelName: 'user',
 }
 );
 
